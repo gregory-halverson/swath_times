493a92485cbd8d504d45cdeb8953b053e36e2c4f
Gregory H. Halverson <gregory.halverson@gmail.com>
2016-05-27 20:05:01 -0700

    initial commit

--- swath_times.py
+++ swath_times.py
@@ -1,7 +1,8 @@
+import os
 from glob import glob
 
 base_path = '/nobackup0/omega/halverso/evapotranspiration_data/sources/MOD07_L2'
 
-date_list = glob(base_path)
+date_list = glob(os.path.join(base_path, '*'))
 
 print(date_list)
