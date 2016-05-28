675a3f6849dd43c7630847ab9812ce8ca9d18aa4
Gregory H. Halverson <gregory.halverson@gmail.com>
2016-05-27 20:07:47 -0700

    initial commit

--- swath_times.py
+++ swath_times.py
@@ -5,6 +5,15 @@ base_path = '/nobackup0/omega/halverso/evapotranspiration_data/sources/MOD07_L2'
 
 date_list = glob(os.path.join(base_path, '*'))
 
+date_list.sort()
+
 for directory in date_list:
     date_string = os.path.basename(directory)
     print(date_string)
+
+    swath_list = glob(os.path.join(directory, '*.hdf'))
+
+    swath_list.sort()
+
+    for filename in swath_list:
+        print(filename)
